7c618a907a0414e229352ce6c28cf2e5cae0ea4d
Pranav R Nayak N <73541801+1001pranav@users.noreply.github.com>
2025-04-11 23:41:04 +0530

    Delete notes/dynamodb-cheatsheet directory

--- notes/dynamodb-cheatsheet/examples/delete-item.js
+++
@@ -1,9 +0,0 @@
-const AWS = require('aws-sdk');
-const docClient = new AWS.DynamoDB.DocumentClient();
-
-const params = {
-  TableName: 'Users',
-  Key: { userId: '123' }
-};
-
-docClient.delete(params).promise();
